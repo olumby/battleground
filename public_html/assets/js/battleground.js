f8baf8c114c1b737dd0f5fd9552baab9f92564e8
Oliver Lumby <me@oliverlumby.com>
2013-12-09 18:31:14 +0100

    Grid, Boundaries, Zoom

--- public_html/assets/js/battleground.js
+++ public_html/assets/js/battleground.js
@@ -1,4 +1,18 @@
-var globalData, zoomInfo, mapInfo, mXPos, mYPos;
+var globalData, zoomInfo, mapInfo, mXPos, mYPos, totalWidth, totalHeight, mCoorX, mCoorY,
+	showGrid = true,
+	showBoundaries = true;
+
+var canvas = document.getElementById('battleground');
+var ctx = canvas.getContext('2d');
+
+var grid_canvas = document.getElementById('battleground-grid');
+var grid_context = grid_canvas.getContext('2d');
+
+var items_canvas = document.getElementById('battleground-items');
+var items_context = items_canvas.getContext('2d');
+
+var boundaries_canvas = document.getElementById('battleground-boundaries');
+var boundaries_context = boundaries_canvas.getContext('2d');
 
 $(document).ready(function() {
 
@@ -10,24 +24,28 @@ $(document).ready(function() {
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
 
+		grid_canvas.width = window.innerWidth;
+		grid_canvas.height = window.innerHeight;
+
+		items_canvas.width = window.innerWidth;
+		items_canvas.height = window.innerHeight;
+
+		boundaries_canvas.width = window.innerWidth;
+		boundaries_canvas.height = window.innerHeight;
+
 		drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
 	}
-
-	var vX = 0, 
-		vY = 0,
-		vWidth = window.innerWidth,
-		vHeight = window.innerHeight;
 
 	var urlVars = getUrlVars(window.location.href);
 	var mapName = urlVars['map'];
 	var baseUrl = "assets/maps/"+mapName+"/minimap_Win32_mip{z}_{x}_{y}.jpg";
 	var zoomLevel = 1;
 
-	var canvas = document.getElementById('battleground');
-	var ctx = canvas.getContext('2d');
-
 	mXPos = 50;
 	mYPos = 50;
+
+	mCoorX = 50;
+	mCoorY = 50;
 
 	var imageRes = 1024;
 
@@ -36,11 +54,13 @@ $(document).ready(function() {
 	$.getJSON("assets/js/data.json", function(data) {
 		globalData = data;
 		zoomInfo = data['zoomInfo'];
-		mapInfo = data['mapInfo'];
+		mapInfo = data['mapInfo'][mapName];
 
 		if(data['mapInfo'][mapName]) {
 			mXPos = data['mapInfo'][mapName]['xCenter'];
-			mYPos = data['mapInfo'][mapName]['yCenter'];
+			mCoorX = data['mapInfo'][mapName]['xCenter'];
+			mYPos =  data['mapInfo'][mapName]['yCenter'];
+			mCoorY = data['mapInfo'][mapName]['yCenter'];
 		}
 
 		resizeCanvas();
@@ -51,10 +71,8 @@ $(document).ready(function() {
 
 	function drawStuff(xloop, yloop, zoom) {
 
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-		var totalWidth = xloop * imageRes;
-		var totalHeight = yloop * imageRes;
+		totalWidth = xloop * imageRes;
+		totalHeight = yloop * imageRes;
 
 		var centerXPx = totalWidth*(mXPos/100)
 		var centerYPx = totalHeight*(mYPos/100)
@@ -64,11 +82,8 @@ $(document).ready(function() {
 		var yStartPx = centerYPx - (canvas.height/2);
 		var yFinishPx = centerYPx + (canvas.height/2);
 
-
 		var startCoor = [Math.floor(xStartPx / imageRes), Math.floor(yStartPx / imageRes)]
 		var finishCoor = [Math.ceil(xFinishPx / imageRes), Math.ceil(yFinishPx / imageRes)]
-
-		console.log (startCoor, finishCoor);
 
 		var coordinates = [];
 
@@ -86,12 +101,8 @@ $(document).ready(function() {
 					"y" : y,
 					"url" : theUrl
 				};
+
 				coordinates.push(urlDict);
-
-				var globalXOffset = xStartPx - (coordinates[0].x * imageRes);
-				var globalYOffset = yStartPx - (coordinates[0].y * imageRes);
-
-
 
 			}
 		}
@@ -102,7 +113,6 @@ $(document).ready(function() {
 		$.each(coordinates, function(index, val) {
 
 			var img = new Image();
-
 
 			if(val.x > (xloop-1) || val.x < 0 || val.y > (yloop-1) || val.y < 0) {
 				img.src = "assets/images/black.png";
@@ -122,102 +132,99 @@ $(document).ready(function() {
 		});
 
 
-		//console.log(xStart, xFinish);
-		//console.log(yStart, yFinish);
+		drawGrid(totalWidth, totalHeight, xStartPx, yStartPx);
+		drawItems(totalWidth, totalHeight, xStartPx, yStartPx);
+		drawBoundaries(totalWidth, totalHeight, xStartPx, yStartPx);
 		
-/*		for(var i = 0; i < xloop; i++) {
-			for(var j = 0; j < yloop; j++) {
-				var theUrl = baseUrl;
-
-				theUrl = theUrl.replace("{x}", i);
-				theUrl = theUrl.replace("{y}", j);
-				theUrl = theUrl.replace("{z}", zoom);
-
-				var urlDict = {
-					"x" : i,
-					"y" : j,
-					"url" : theUrl
-				};
-
-				urlArray.push(urlDict);
-			}
-		}
-
-		var totalWidth = xloop * imageRes;
-		var totalHeight = yloop * imageRes;
-
-		var centerX = totalWidth*(mXPos/100)
-		var centerY = totalHeight*(mYPos/100)
-
-		var xStart = centerX - (window.innerWidth/2);
-		var xFinish = centerX + (window.innerWidth/2);
-		var yStart = centerY - (window.innerHeight/2);
-		var yFinish = centerY + (window.innerHeight/2);
-
-		var aPoint,
-			bPoint;
-
-		$.each(urlArray, function(index, val) {
-
-			if( (val.x * imageRes) < xStart && (val.x * imageRes) + imageRes > xStart ) {
-				if( (val.y * imageRes) < yStart && (val.y * imageRes) + imageRes > yStart ) {
-					aPoint = [val.x, val.y];
-				}
-			}
-
-			if( (val.x * imageRes) < xFinish && (val.x * imageRes) + imageRes > xFinish ) {
-				if( (val.y * imageRes) < yFinish && (val.y * imageRes) + imageRes > yFinish ) {
-					bPoint = [val.x, val.y];
-				}
-			}
-
-		});
-
-		var coordinates = [];
-
-		for (var x = aPoint[0]; x <= bPoint[0]; x++) {
-			for (var y = aPoint[1]; y <= bPoint[1]; y++) {
-
-				var theUrl = baseUrl;
-
-				theUrl = theUrl.replace("{x}", x);
-				theUrl = theUrl.replace("{y}", y);
-				theUrl = theUrl.replace("{z}", zoom);
-
-				var urlDict = {
-					"x" : x,
-					"y" : y,
-					"url" : theUrl
-				};
-				coordinates.push(urlDict);
-			}
-		}
-
-
-		var globalXOffset = xStart - (coordinates[0].x * imageRes);
-		var globalYOffset = yStart - (coordinates[0].y * imageRes);
-
-		$.each(coordinates, function(index, val) {
-
-			console.log(mXPos, mXPos);
-			
-			var img = new Image();
-
-			img.src = val.url;
-			img.width = imageRes;
-			img.height = imageRes;
-
-			var xOffset = (imageRes * (val.x - coordinates[0].x))-globalXOffset;
-			var yOffset = (imageRes * (val.y - coordinates[0].y))-globalYOffset;
-			img.onload = function() {
-				ctx.drawImage(img, xOffset, yOffset, imageRes, imageRes);
-			}
-
-		});
-
-*/
+		updateUI();
 
 	}
+
+
+	function drawGrid(width, height, xOffset, yOffset) {
+
+		grid_canvas.width = grid_canvas.width;
+		grid_context.clearRect(0, 0, grid_canvas.width, grid_canvas.height);
+
+		if(showGrid) {
+
+			for (var x = 0; x < width; x += zoomInfo[zoomLevel]['grid']) {
+				grid_context.moveTo(x-xOffset, 0);
+				grid_context.lineTo(x-xOffset, height);
+			}
+
+			for (var y = 0; y < height; y += zoomInfo[zoomLevel]['grid']) {
+				grid_context.moveTo(0, y-yOffset);
+				grid_context.lineTo(width, y-yOffset);
+			}
+
+			grid_context.strokeStyle = "#fff";
+			grid_context.stroke();
+
+		}
+
+	}
+
+	function drawItems(width, height, xOffset, yOffset) {
+
+		items_context.clearRect(0, 0, items_canvas.width, items_canvas.height);
+
+		if(mapInfo) {
+			var tempArr = mapInfo['gametypes']['ConquestLarge0']['points'];
+
+			$.each(tempArr, function(index, val) {
+
+				var theimg = new Image();
+				theimg.src = val.resource;
+
+				var points = val.coors;
+
+				theimg.onload = function() {
+
+					var xo = (width*(points[0]/100))-xOffset-(this.width/2);
+					var yo = (height*(points[1]/100))-yOffset-(this.height/2);
+					items_context.drawImage(theimg, xo, yo, theimg.width, theimg.height);
+
+				}
+
+			});
+		}
+
+	}
+
+	function drawBoundaries(width, height, xOffset, yOffset) {
+
+		boundaries_canvas.width = boundaries_canvas.width;
+		boundaries_context.clearRect(0, 0, boundaries_canvas.width, boundaries_canvas.height);
+
+		if(showBoundaries) {
+			boundaries_context.fillStyle = "rgba(0,0,0,.5)";
+			boundaries_context.fillRect(0, 9, boundaries_canvas.width, boundaries_canvas.height);
+
+			boundaries_context.globalCompositeOperation = 'destination-out';
+
+			boundaries_context.fillStyle = "#fff";
+			boundaries_context.strokeStyle = "#fff";
+			boundaries_context.beginPath();
+			
+			boundaries_context.moveTo( width*(53/100)-xOffset , height*(35/100)-yOffset );
+			boundaries_context.lineTo( width*(50/100)-xOffset , height*(44/100)-yOffset );
+			boundaries_context.lineTo( width*(50/100)-xOffset , height*(51/100)-yOffset );
+			boundaries_context.lineTo( width*(52/100)-xOffset , height*(57/100)-yOffset );
+			boundaries_context.lineTo( width*(55/100)-xOffset , height*(63/100)-yOffset );
+			boundaries_context.lineTo( width*(65/100)-xOffset , height*(64/100)-yOffset );
+			boundaries_context.lineTo( width*(67/100)-xOffset , height*(57/100)-yOffset );
+			boundaries_context.lineTo( width*(68/100)-xOffset , height*(45/100)-yOffset );
+			boundaries_context.lineTo( width*(66/100)-xOffset , height*(33/100)-yOffset );
+
+			boundaries_context.closePath();
+
+			boundaries_context.fill();
+			boundaries_context.stroke();
+		}
+	
+	}
+
 
 	// Setup mouse dragging
 	
@@ -235,6 +242,15 @@ $(document).ready(function() {
 	});
 
 	$('#battleground').mousemove(function(e) {
+
+		var centerXPx = totalWidth*(mXPos/100)
+		var centerYPx = totalHeight*(mYPos/100)
+
+		var xStartPx = centerXPx - (canvas.width/2);
+		var yStartPx = centerYPx - (canvas.height/2);
+
+		mCoorX = ((xStartPx + e.clientX)/totalWidth)*100;
+		mCoorY = ((yStartPx + e.clientY)/totalHeight)*100;
 
 		if (clicking) {
 			e.preventDefault();
@@ -261,11 +277,62 @@ $(document).ready(function() {
 			}
 
 			drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
-
-			updateUI();
 			
+		} else {
+			updateUI();
 		}
+
 	});
+
+	/*
+	 *  Setup Zoom
+	 */
+
+	// Browser
+	// Firefox Scroll
+	$('#battleground').bind('DOMMouseScroll', function(e){
+		if(e.originalEvent.detail > 0) {
+			doZoom(false);
+		} else {
+			doZoom(true);
+		}
+		return false;
+	});
+
+	// Webkit, etc..
+	$('#battleground').bind('mousewheel', function(e){
+		if(e.originalEvent.wheelDelta < 0) {
+			doZoom(false);
+		} else {
+			doZoom(true);
+		}
+		return false;
+	});
+
+	// Buttons
+	$('.ui-zoom-in').click(function(e) {
+		doZoom(true);
+		return false;
+	});
+
+	$('.ui-zoom-out').click(function(e) {
+		doZoom(false);
+		return false;
+	});
+
+	function doZoom(dir) {
+		if(dir) {
+			if(zoomInfo[zoomLevel-1]) {
+				zoomLevel = zoomLevel-1;
+			}
+		} else {
+			if(zoomInfo[zoomLevel+1]) {
+				zoomLevel = zoomLevel+1;
+			}
+		}
+		drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
+		//$('.ui-zoom-level').html(zoomInfo[zoomLevel]['label']+"");
+	}
 
 	// Other Functions
 
@@ -282,23 +349,51 @@ $(document).ready(function() {
 
 	function updateUI() {
 		$('.ui-zoom-level').html(zoomInfo[zoomLevel]['label']+"");
-		$('.ui-coor-text').html(''+mXPos.toFixed(2)+', '+mYPos.toFixed(2));
+		$('.ui-coor-text').html(''+mCoorX.toFixed(2)+', '+mCoorY.toFixed(2));
+		if(showGrid) {
+			$('.ui-grid-on').addClass("active");
+			$('.ui-grid-off').removeClass("active");
+		} else {
+			$('.ui-grid-off').addClass("active");
+			$('.ui-grid-on').removeClass("active");
+		}
+		if(showBoundaries) {
+			$('.ui-boundaries-on').addClass("active");
+			$('.ui-boundaries-off').removeClass("active");
+		} else {
+			$('.ui-boundaries-off').addClass("active");
+			$('.ui-boundaries-on').removeClass("active");
+		}
 	}
 
-	$('.ui-zoom-in').click(function(e) {
-		if(zoomInfo[zoomLevel-1]) {
-			zoomLevel = zoomLevel-1;
-			drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
-			$('.ui-zoom-level').html(zoomInfo[zoomLevel]['label']+"");
+	$('.ui-grid-on').click(function(e) {
+		if(!showGrid) {
+			showGrid = true;
 		}
+		drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
 	});
 
-	$('.ui-zoom-out').click(function(e) {
-		if(zoomInfo[zoomLevel+1]) {
-			zoomLevel = zoomLevel+1;
-			drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
-			$('.ui-zoom-level').html(zoomInfo[zoomLevel]['label']+"");
+	$('.ui-grid-off').click(function(e) {
+		if(showGrid) {
+			showGrid = false;
 		}
+		drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
+	});
+
+
+
+	$('.ui-boundaries-on').click(function(e) {
+		if(!showBoundaries) {
+			showBoundaries = true;
+		}
+		drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
+	});
+
+	$('.ui-boundaries-off').click(function(e) {
+		if(showBoundaries) {
+			showBoundaries = false;
+		}
+		drawStuff(zoomInfo[zoomLevel]['x'],zoomInfo[zoomLevel]['y'],zoomLevel);
 	});
 
 });
